bdf69460da5be513c04535c84b7530b2b4161a6b
Giovanni Antoniazzi <giovi@10-192-215-32client.eduroam.upc.edu>
2023-05-08 19:06:48 +0200

    - Updated login.html form to use the correct action attribute (route) - Removed the password input field from login.html - Modified routes.py to handle login, class_data, and submit_vote routes - Added 'add_vote' method to the models.py Class model---VOTING STILL NMOT WORKING THO

--- app/models.py
+++ app/models.py
@@ -2,38 +2,43 @@ from app import db
 from sqlalchemy.orm import relationship
 
 class Votation(db.Model):
-	__tablename__ = "votation"
+    __tablename__ = "votation"
 
-	id = db.Column(db.Integer, primary_key=True)
-	date = db.Column(db.DateTime)
-	voting_class = relationship('Class')
-	results = db.Column(db.ARRAY(db.Integer))
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
+    voting_class = relationship('Class', back_populates='votations')
+    results = db.Column(db.ARRAY(db.Integer))
 
+    def add_vote(self, transport_mode_index):
+        self.results[transport_mode_index] += 1
 
 class Class(db.Model):
+    __tablename__ = "class"
 
-	__tablename__ = "class"
-
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(100))
-	nstudents = db.Column(db.Integer)
-	school = relationship('School')
-	last_votation = db.Column(db.Integer)
-	avg_results = db.Column(db.ARRAY(db.Integer))
-	
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    nstudents = db.Column(db.Integer)
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.id'))
+    school = relationship('School', back_populates='classes')
+    last_votation_id = db.Column(db.Integer, db.ForeignKey('votation.id'))
+    last_votation = relationship('Votation', uselist=False, post_update=True)
+    avg_results = db.Column(db.ARRAY(db.Integer))
+    votations = relationship('Votation', back_populates='voting_class')
 
 class School(db.Model):
-	__tablename__ = 'schools'
+    __tablename__ = 'schools'
 
-	id = db.Column(db.Integer,primary_key=True)
-	league = relationship('League')
+    id = db.Column(db.Integer, primary_key=True)
+    league_id = db.Column(db.Integer, db.ForeignKey('league.id'))
+    league = relationship('League', back_populates='schools')
+    classes = relationship('Class', back_populates='school')
 
 class League(db.Model):
+    __tablename__ = 'league'
 
-	__tablename__ = 'league'
-
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(100))
-	start_time = db.Column(db.DateTime)
-	end_time = db.Column(db.DateTime)
-
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    schools = relationship('School', back_populates='league')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,63 @@
 import flask
-from flask import render_template,url_for,request,redirect
+from flask import flash, session
+from app.models import Class  # Updated to import the Class model from models.py
+from flask import render_template, url_for, request, redirect
 from app import db
 from app import app
 
 @app.route('/')
-def login():
+def home():  # Renamed to avoid conflicting with the other 'login' function
     return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+
+    # Perform a query to find the class with the provided username
+    class_data = Class.query.filter_by(name=username).first()  # Updated the query
+
+    if class_data:
+        # Save the class_id in the session
+        session['class_id'] = class_data.id
+
+        # Redirect to the class_data page for the specific class
+        return redirect(url_for('class_data'))
+    else:
+        flash('Invalid username', 'error')
+        return redirect(url_for('home'))
+
+
+@app.route('/class_data')
+def class_data():
+    class_id = session.get('class_id')
+    if not class_id:
+        flash('Please log in first', 'error')
+        return redirect(url_for('home'))
+
+    # Fetch the class data using the class_id
+    class_data = Class.query.get(class_id)
+
+    return render_template('class_data.html', class_data=class_data)
+
+
+@app.route('/submit_vote', methods=['POST'])
+def submit_vote():
+    class_id = session.get('class_id')
+    if not class_id:
+        flash('Please log in first', 'error')
+        return redirect(url_for('home'))
+
+    # Fetch the class data using the class_id
+    class_data = Class.query.get(class_id)
+
+    # Update the class_data with the submitted vote
+    transport_mode = int(request.form['transport_mode'])  # Convert the submitted value to an integer
+    # Assuming you have a method called `add_vote` in your Votation model
+    class_data.last_votation.add_vote(transport_mode)  # Updated to use last_votation
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    # Redirect to the class_data page
+    return redirect(url_for('class_data'))
