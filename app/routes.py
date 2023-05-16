e2650451ff0d71cd0f83dcfb33aec5aceb9ea684
Giovanni Antoniazzi <giovi@10-192-114-135client.eduroam.upc.edu>
2023-05-16 19:09:20 +0200

    added a new route /getdata to retrieve votes real time every time a new vote is sent

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
-from flask import flash, session, render_template, redirect, url_for, request
+from flask import flash, session, render_template, redirect, url_for, request, jsonify
 from app.models import Class, Votation
 from app import db, app
 import datetime
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 
 @app.route('/')
 def home():
@@ -42,34 +47,76 @@ def class_data():
 
 @app.route('/submit_vote', methods=['POST'])
 def submit_vote():
-    class_id = session.get('class_id')
-    if not class_id:
-        flash('Please log in first', 'error')
-        return redirect(url_for('home'))
+    try:
+        class_id = session.get('class_id')
+        if not class_id:
+            flash('Please log in first', 'error')
+            return redirect(url_for('home'))
 
-    today = datetime.datetime.now(datetime.timezone.utc).date()
-    transport_mode = request.form['transport_mode']
-    votation_data = Votation.query.filter(Votation.date == today, Votation.class_id == class_id).first()
+        today = datetime.datetime.now(datetime.timezone.utc).date()
+        transport_mode = request.form.get('transport_mode')  # change this line
 
-    if not votation_data:
-        votation_data = Votation(date=today, class_id=class_id, nwalking=0, ncycling=0, ncar=0,
-                                 npublic_transport=0, ncarpooling=0, nothers=0)
-        db.session.add(votation_data)
+        # Check if transport_mode is not None
+        if not transport_mode:
+            flash('Transport mode not provided', 'error')
+            return redirect(url_for('class_data'))
+
+        votation_data = Votation.query.filter(Votation.date == today, Votation.class_id == class_id).first()
+
+        if not votation_data:
+            votation_data = Votation(date=today, class_id=class_id, nwalking=0, ncycling=0, ncar=0,
+                                     npublic_transport=0, ncarpooling=0, nothers=0)
+            db.session.add(votation_data)
+            db.session.commit()
+
+        if transport_mode == 'foot':
+            votation_data.nwalking += 1
+        elif transport_mode == 'bike':
+            votation_data.ncycling += 1
+        elif transport_mode == 'car':
+            votation_data.ncar += 1
+        elif transport_mode == 'bus':
+            votation_data.npublic_transport += 1
+        elif transport_mode == 'carpooling':
+            votation_data.ncarpooling += 1
+        elif transport_mode == 'other':
+            votation_data.nothers += 1
+
         db.session.commit()
 
-    if transport_mode == 'foot':
-        votation_data.nwalking += 1
-    elif transport_mode == 'bike':
-        votation_data.ncycling += 1
-    elif transport_mode == 'car':
-        votation_data.ncar += 1
-    elif transport_mode == 'bus':
-        votation_data.npublic_transport += 1
-    elif transport_mode == 'carpooling':
-        votation_data.ncarpooling += 1
-    elif transport_mode == 'other':
-        votation_data.nothers += 1
+        return redirect(url_for('class_data'))
 
-    db.session.commit()
+    except Exception as e:
+        logging.exception("Exception occurred")
+        print(e)  # printing out the exception message
+        return "An error occurred while submitting vote", 500  # return a user-friendly error message
 
-    return redirect(url_for('class_data'))
+from flask import jsonify
+
+@app.route('/get_vote_data', methods=['GET'])
+def get_vote_data():
+    try:
+        class_id = session.get('class_id')
+        if not class_id:
+            return jsonify({'error': 'Please log in first'}), 401
+
+        today = datetime.datetime.now(datetime.timezone.utc).date()
+        votation_data = Votation.query.filter(Votation.date == today, Votation.class_id == class_id).first()
+
+        if not votation_data:
+            return jsonify({'error': 'No voting data available'}), 404
+
+        data = {
+            'foot': votation_data.nwalking,
+            'bike': votation_data.ncycling,
+            'car': votation_data.ncar,
+            'bus': votation_data.npublic_transport,
+            'carpooling': votation_data.ncarpooling,
+            'other': votation_data.nothers,
+        }
+
+        return jsonify(data)
+
+    except Exception as e:
+        logging.exception("Exception occurred")
+        return jsonify({'error': 'An error occurred while getting vote data'}), 500
