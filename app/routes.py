a0df75be9d985d9bec6d65b94adaad0d76093fb0
Giovanni Antoniazzi <giovi@giovannis-macbook-air.home>
2023-05-22 21:11:58 +0200

    Added graphical improvements, implemented delete vote button

--- app/routes.py
+++ app/routes.py
@@ -31,9 +31,15 @@ def class_data():
         return redirect(url_for('home'))
 
     class_data = Class.query.get(class_id)
-    today = datetime.datetime.now(datetime.timezone.utc).date()
     
-    votation_data = Votation.query.filter(Votation.date == today and Votation.class_id == class_id).first()
+    # Get the selected date from the request's query parameters
+    selected_date = request.args.get('date-filter')
+    if selected_date:
+        selected_date = datetime.datetime.strptime(selected_date, '%Y-%m-%d').date()
+    else:
+        selected_date = datetime.datetime.now(datetime.timezone.utc).date()
+
+    votation_data = Votation.query.filter(Votation.date == selected_date, Votation.class_id == class_id).first()
     transport_list = []
     score = '0'
     if votation_data:
@@ -42,11 +48,11 @@ def class_data():
         if total_transport != 0:
             transport_list = [votation_data.nwalking, votation_data.ncycling, votation_data.ncar,
                               votation_data.npublic_transport, votation_data.ncarpooling, votation_data.nothers]
-            transport_list = [round((x * 100) / total_transport, 2) for x in transport_list]
         
         score = str(votation_data.score)
 
-    return render_template('class_data.html', class_data=class_data, transport_list=transport_list, score = score)
+    return render_template('class_data.html', class_data=class_data, transport_list=transport_list, score=score, selected_date=selected_date)
+
 
 @app.route('/submit_vote', methods=['POST'])
 def submit_vote():
@@ -66,19 +72,18 @@ def submit_vote():
         
         votation_data = Votation.query.filter(Votation.date == today, Votation.class_id == class_id).first()
 
-        #When there's no votation data for today, create it
+        # When there's no votation data for today, create it
         if not votation_data:
             votation_data = Votation(date=today, class_id=class_id, nwalking=0, ncycling=0, ncar=0,
-                                     npublic_transport=0, ncarpooling=0, nothers=0, score = 0)
+                                     npublic_transport=0, ncarpooling=0, nothers=0, score=0)
             db.session.add(votation_data)
-            db.session.commit()
 
         if transport_mode == 'foot':
             votation_data.nwalking += 1
             votation_data.score += 25
         elif transport_mode == 'bike':
             votation_data.ncycling += 1
-            votation_data.score +=25
+            votation_data.score += 25
         elif transport_mode == 'car':
             votation_data.ncar += 1
             votation_data.score += 5
@@ -108,8 +113,9 @@ def get_vote_data():
         if not class_id:
             return jsonify({'error': 'Please log in first'}), 401
 
-        today = datetime.datetime.now(datetime.timezone.utc).date()
-        votation_data = Votation.query.filter(Votation.date == today, Votation.class_id == class_id).first()
+        date = request.args.get('date')
+        selected_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+        votation_data = Votation.query.filter(Votation.date == selected_date, Votation.class_id == class_id).first()
 
         if not votation_data:
             return jsonify({'error': 'No voting data available'}), 404
@@ -128,3 +134,48 @@ def get_vote_data():
     except Exception as e:
         logging.exception("Exception occurred")
         return jsonify({'error': 'An error occurred while getting vote data'}), 500
+
+@app.route('/delete_last_vote', methods=['POST'])
+def delete_last_vote():
+    try:
+        class_id = session.get('class_id')
+        if not class_id:
+            return jsonify({'error': 'Please log in first'}), 401
+
+        today = datetime.datetime.now(datetime.timezone.utc).date()
+
+        # Find the last vote for the current date and class_id
+        votation_data = Votation.query.filter(
+            Votation.date == today, Votation.class_id == class_id
+        ).order_by(Votation.id.desc()).first()
+
+        if not votation_data:
+            return jsonify({'error': 'No vote to delete'}), 404
+
+        # Decrement the count and score based on the transport mode of the last vote
+        if votation_data.nothers > 0:
+            votation_data.nothers -= 1
+            votation_data.score -= 5
+        elif votation_data.ncarpooling > 0:
+            votation_data.ncarpooling -= 1
+            votation_data.score -= 10
+        elif votation_data.npublic_transport > 0:
+            votation_data.npublic_transport -= 1
+            votation_data.score -= 15
+        elif votation_data.ncar > 0:
+            votation_data.ncar -= 1
+            votation_data.score -= 5
+        elif votation_data.ncycling > 0:
+            votation_data.ncycling -= 1
+            votation_data.score -= 25
+        elif votation_data.nwalking > 0:
+            votation_data.nwalking -= 1
+            votation_data.score -= 25
+
+        db.session.commit()
+
+        return jsonify({'success': True})
+
+    except Exception as e:
+        logging.exception("Exception occurred")
+        return jsonify({'error': 'An error occurred while deleting the last vote'}), 500
